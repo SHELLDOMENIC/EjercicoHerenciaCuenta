0430eff32f4552b0b632dd1b4714c5b70199b2fb
Stalyn0101 <yixon11@gmail.com>
2024-12-18 15:00:26 -0500

    holaa

--- src/main/java/com/mycompany/ejercicioherenciaejerciciod/clase/CuentaCorriente.java
+++ src/main/java/com/mycompany/ejercicioherenciaejerciciod/clase/CuentaCorriente.java
@@ -5,9 +5,53 @@
 package com.mycompany.ejercicioherenciaejerciciod.clase;
 
 
-public class CuentaCorriente {
-    CuentaCorriente cuenta = new 
-        CuentaCorriente (" Andrea Chaquinga ", 2000);
+public class CuentaCorriente extends EjercicioHerenciaEjerciciod {
+    public class CuentaCorriente extends EjercicioHerenciaEjerciciod {
+    private float sobregiro;
+
+    public CuentaCorriente(float saldo, float tasaAnual) {
+        super(saldo, tasaAnual);
+        sobregiro = 0;
+    }
+
+    @Override
+    public void retirar(float monto) {
+        saldo -= monto;
+        if (saldo < 0) {
+            sobregiro = -saldo;
+            saldo = 0;
+        }
+    }
+
+    @Override
+    public void consignar(float monto) {
+        super.consignar(monto);
+        if (sobregiro > 0) {
+            if (monto > sobregiro) {
+                saldo += monto - sobregiro;
+                sobregiro = 0;
+            } else {
+                sobregiro -= monto;
+                monto = 0;
+            }
+        }
+    }
+
+    @Override
+    public void imprimir() {
+        System.out.println("Saldo: " + saldo);
+        System.out.println("Comisión mensual: " + comisionMensual);
+        System.out.println("Número de transacciones: " + (numConsignaciones + numRetiros));
+        System.out.println("Sobregiro: " + sobregiro);
+    }
+}
+
+
+      
+}
+
+   
+   
     
     
     
