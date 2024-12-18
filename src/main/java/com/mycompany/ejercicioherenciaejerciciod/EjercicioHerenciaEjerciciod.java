c58c7f8c44480c25d0cfd4dfc1797636c2895f0a
ShellDomenic <domenicaloor177@gmail.com>
2024-12-18 14:16:47 -0500

    DOmenica

--- src/main/java/com/mycompany/ejercicioherenciaejerciciod/EjercicioHerenciaEjerciciod.java
+++ src/main/java/com/mycompany/ejercicioherenciaejerciciod/EjercicioHerenciaEjerciciod.java
@@ -6,5 +6,6 @@ public class EjercicioHerenciaEjerciciod {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("rama principal, me toca");
     }
 }
