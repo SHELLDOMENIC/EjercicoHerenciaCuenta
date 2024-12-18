3cebe2ef72957939a8b68f8c3550eda765568954
Stalyn0101 <yixon11@gmail.com>
2024-12-18 14:57:49 -0500

    hola

---
+++ src/main/java/com/mycompany/ejercicioherenciaejerciciod/clase/CuentaCorriente.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ejercicioherenciaejerciciod.clase;
+
+
+public class CuentaCorriente {
+    CuentaCorriente cuenta = new 
+        CuentaCorriente (" Andrea Chaquinga ", 2000);
+    
+    
+    
+    
+}
